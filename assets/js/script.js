086bd07010426dd8163844861973facbd5eeaada
ommarkhawaja1 <ommarkhawaja1@gmail.com>
2022-06-01 21:31:26 -0400

    call genre api, call games api, loop over the games that we get back

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 
 $("#game-btn").on("click", gameInputHandler);
 $("#genre-btn").on("click", genreInputHandler);
+var gamesInfo = [];
 
 function gameInputHandler() {
   var gameInput = $("#game-input").val().trim();
@@ -59,31 +60,91 @@ function gameFetchResponse(gameInput) {
 //fetch and response handling for genre search (use a different API, probably gamebomb) (does not currently work)
 //my user key for giantbomb: 74396db661dc842e2e30773ee2aa76fbd447cbc1
 //----------------------------------------------------------------------------------------
-function genreFetchResponse(yearInput) {
-  var yearFetchUrl =
-  "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/games/?api_key=74396db661dc842e2e30773ee2aa76fbd447cbc1&limit=10&format=json&filter=original_release_date:" +
-  yearInput +
-  "-01-01|" +
-  yearInput +
-  "-12-31"
 
-  const options = {
+// 1. Call the genres api to get all genres (name, guid)
+function genreFetchResponse(genreInput) {
+  var genreFetchUrl =
+    "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/genres/?api_key=74396db661dc842e2e30773ee2aa76fbd447cbc1&field_list=guid,name"
+
+  var options = {
     method: "GET",
     headers: {
+      "Content-Type": "application/xml",
       "Access-Control-Allow-Origin": "http://127.0.0.1:5500/",
-      "GiantBomb-Host": "https://www.giantbomb.com/api/",
-      "GiantBomb-Key": "74396db661dc842e2e30773ee2aa76fbd447cbc1",
+      "X-RapidAPI-Host": "https://www.giantbomb.com/api/",
+      "X-RapidAPI-Key": "74396db661dc842e2e30773ee2aa76fbd447cbc1",
     },
   };
 
-  fetch(yearFetchUrl, options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+  fetch(genreFetchUrl, options)
+    // .then((response) => response.json())
+    .then((response) => console.log(response.text()))
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => console.log(data))
     .catch((err) => console.error(err)); //200 error (can't connect)
 
   //if response works, genreSearchHandler
   //if 400, genreNotFoundHandler
+
+  // 2. Call the games api (limit 50)
+  var gamesFetchUrl =
+    "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/games/?api_key=073c2f94ba69540e99d2b7e8b4cd3aebb2d9befb&format=json&sort=number_of_user_reviews:desc&limit=50&field_list=guid,id,name,aliases"
+
+  options = {
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "http://127.0.0.1:5500/",
+      "X-RapidAPI-Host": "https://www.giantbomb.com/api/",
+      "X-RapidAPI-Key": "74396db661dc842e2e30773ee2aa76fbd447cbc1",
+    },
+  };
+  var gamesResponse = fetch(gamesFetchUrl, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+  fetch(gamesFetchUrl, options)
+    .then((gamesResponse) => gamesResponse.json())
+    .then((gamesResponse) => console.log(gamesResponse))
+    .catch((err) => console.error(err));
+
+
+  // 3. Iterate/loop over the games that we get back
+  for (i = 0; i < 50; i++) {
+    //    a. Call Game api to get more details about the game, gives us the genre for that game
+    var gameFetchUrl =
+      "https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/game/" + gamesResponse.results[i].guid + "/?api_key=74396db661dc842e2e30773ee2aa76fbd447cbc1&format=json&field_list=genres,name"
+
+    options = {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "http://127.0.0.1:5500/",
+        "X-RapidAPI-Host": "https://www.giantbomb.com/api/",
+        "X-RapidAPI-Key": "74396db661dc842e2e30773ee2aa76fbd447cbc1",
+      },
+    };
+
+    fetch(gameFetchUrl, options)
+      .then((gameResponse) => gameResponse.json())
+      .then((gameResponse) => console.log(gameResponse))
+      .catch((err) => console.error(err));
+
+    for (j = 0; j < gameResponse.results.genres.length; j++) {
+      //    b. If the genre matches what we are looking for, add that to an array
+      if (gameResponse.results.genre[j].name == genreInput) {
+
+        gamesInfo.push(gamesResponse.results[i])
+        break
+      }
+    }
+    //    c. Keep looping until we have the number games we want to show (like we want to present the user with 10 action games, loop until we have 10 action games in our array)
+    if (gamesInfo.length > 9) {
+      break
+    }
+  }
+
 }
+
 
 //----------------------------------------------------------------------------------------
 
