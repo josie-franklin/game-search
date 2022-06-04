3e4b7cdfb770bf0dbdac38afbb7d188349b91253
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-06-04 16:33:34 -0400

    cleaned code, add local storage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 //init function that displays previous searches from local storage
-//var savedSearchesObj = JSON.parse(localStorage.getItem("savedSearchesObj")) || [];
+function init() {
+  // get the saved game array
+  var savedGamesObj = JSON.parse(localStorage.getItem("savedGamesObj")) || [];
+  savedGamesObj.forEach(function (game) {
+    var savedGameContainer = $("#saved-games");
+    var newSavedGameButton = $("<button>")
+      .text(game)
+      .attr("id", game)
+      .on("click", fetchGameId);
+    savedGameContainer.append(newSavedGameButton);
+  });
+}
+init();
 
 $("#game-btn").on("click", gameInputHandler);
 $("#genre-btn").on("click", genreInputHandler);
-var gamesInfo = [];
 
 function gameInputHandler() {
-  $('#review-container').text("");
+  $("#review-container").text("");
   var gameInput = $("#game-input").val().trim();
   if (gameInput === "" || null) {
     //alert text if nothing is inputted
@@ -17,7 +28,6 @@ function gameInputHandler() {
     gameFetchResponse(gameInput);
   }
 }
-
 
 function genreInputHandler(event) {
   event.preventDefault();
@@ -60,10 +70,7 @@ function gameFetchResponse(gameInput) {
   }
 }
 
-//fetch and response handling for genre search (use a different API, probably gamebomb) (does not currently work)
 //my user key for giantbomb: 74396db661dc842e2e30773ee2aa76fbd447cbc1
-//----------------------------------------------------------------------------------------
-
 // 1. Call the genres api to get all genres (name, guid)
 function genreFetchResponse(genreInput) {
   var genreFetchUrl =
@@ -84,9 +91,6 @@ function genreFetchResponse(genreInput) {
       console.log(data);
     })
     .catch((err) => console.error(err));
-
-  //if response works, genreSearchHandler
-  //if 400, genreNotFoundHandler
 
   // 2. Call the games api (limit 50)
   var gamesFetchUrl =
@@ -134,7 +138,6 @@ function genreFetchResponse(genreInput) {
         if (gameGenres !== undefined) {
           gameGenres.forEach(function (genre) {
             if (genre.name == genreInput) {
-              console.log("match", gameResponse.results.name);
               var genreContainer = $("#genre-container");
               var genreTitleEl = $("<p>")
                 .text(gameResponse.results.name)
@@ -145,26 +148,9 @@ function genreFetchResponse(genreInput) {
           });
         }
       }
-
-      // for (j = 0; j < gameResponse.results.genres.length; j++) {
-      //   //    b. If the genre matches what we are looking for, add that to an array
-      //   // console.log(gameResponse.results.genres[0].name);
-      //   if (gameResponse.results.genres[0].name == genreInput) {
-      //     gamesInfo.push(gamesResponse.results[i]);
-      //     console.log(gamesInfo);
-      //     break;
-      //   }
-      // }
     }
-
-    // //    c. Keep looping until we have the number games we want to show (like we want to present the user with 10 action games, loop until we have 10 action games in our array)
-    // if (gamesInfo.length > 9) {
-    //   break;
-    // }
   }
 }
-
-//----------------------------------------------------------------------------------------
 
 //gameSearchHandler
 function gameSearchHandler(gameData) {
@@ -172,21 +158,6 @@ function gameSearchHandler(gameData) {
   $("#game-alert-container").text("");
   //empty the search result container
   var searchResultContainer = $("#game-container").text("");
-  //get the searched game
-  var gameSearch = $("#game-input").val().trim();
-  //get the saved searches from local storage, or an empty array if there isn't one
-  var savedSearchesObj =
-    JSON.parse(localStorage.getItem("savedSearchesObj")) || [];
-  //filter out names that match what was inputted, to avoid duplicates (!!! CURRENTLY SEES CAPS AS DIFFERENT FROM LOWERCASE)
-  savedSearchesObj = savedSearchesObj.filter(function (names) {
-    return names !== gameSearch;
-  });
-  //push the inputted name and save to local storage
-  savedSearchesObj.push(gameSearch);
-  localStorage.setItem("savedSearchesObj", JSON.stringify(savedSearchesObj));
-
-  //TODO: display new saved search to the saved searches buttons
-  //TODO: if saved search already exists, don't create a new button
 
   //display search results, and add an avant listener to each result
   gameData.forEach(function (game) {
@@ -201,6 +172,7 @@ function gameSearchHandler(gameData) {
 //fetchGameId
 function fetchGameId(event) {
   var clickedGameTitle = event.target.textContent;
+  createSavedGameButton(clickedGameTitle);
   var reviewFetchUrl =
     "https://whatoplay.p.rapidapi.com/search?game=" + clickedGameTitle;
 
@@ -216,6 +188,39 @@ function fetchGameId(event) {
     .then((response) => response.json())
     .then((response) => getGameId(response))
     .catch((err) => console.error(err)); //200 error (can't connect)
+}
+
+function createSavedGameButton(gameTitle) {
+  //get the saved searches from local storage, or an empty array if there isn't one
+  var savedGamesObj = JSON.parse(localStorage.getItem("savedGamesObj")) || [];
+  //filter out names that match what was inputted, to avoid duplicates
+  savedGamesObj = savedGamesObj.filter(function (names) {
+    return names !== gameTitle;
+  });
+  //push the inputted name and save to local storage
+  savedGamesObj.push(gameTitle);
+  localStorage.setItem("savedGamesObj", JSON.stringify(savedGamesObj));
+
+  //TODO: if saved search already exists, don't create a new button
+  var sameSavedGame = document.getElementById(gameTitle);
+  if (sameSavedGame === null || undefined) {
+    //get the saved game container
+    var savedGameContainer = $("#saved-games");
+    var newSavedGameButton = $("<button>")
+      .text(gameTitle)
+      .attr("id", gameTitle)
+      .on("click", fetchGameId);
+    savedGameContainer.append(newSavedGameButton);
+  } else {
+    sameSavedGame.remove();
+    //get the saved game container
+    var savedGameContainer = $("#saved-games");
+    var newSavedGameButton = $("<button>")
+      .text(gameTitle)
+      .attr("id", gameTitle)
+      .on("click", fetchGameId);
+    savedGameContainer.append(newSavedGameButton);
+  }
 }
 
 function getGameId(data) {
@@ -245,64 +250,71 @@ function fetchReview(gameId) {
 
   function getObjKeys(info) {
     reviewInfo = Object.values(info)[0];
-    console.log(reviewInfo);
-    $('#game-container').text("");
-    $('#genre-container').text("");
-    var reviewContainerEl = $('#review-container');
+    $("#game-container").text("");
+    $("#genre-container").text("");
+    var reviewContainerEl = $("#review-container");
 
     //display game name data.game_name
-    var gameReviewTitleEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text(reviewInfo.data.game_name);
+    var gameReviewTitleEl = $("<p>")
+      .addClass("border-slate-900 text-center decoration-white")
+      .text(reviewInfo.data.game_name);
     reviewContainerEl.append(gameReviewTitleEl);
 
     //display overall score
 
-    if(reviewInfo.data.gamerscore === null) {
-    var gameReviewTitleEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('No review score was found.');
+    if (reviewInfo.data.gamerscore === null) {
+      var gameReviewTitleEl = $("<p>")
+        .addClass("border-slate-900 text-center decoration-white")
+        .text("No review score was found.");
     } else {
-      var gameReviewTitleEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('Review Score: ', reviewInfo.data.gamerscore);
+      var gameReviewTitleEl = $("<p>")
+        .addClass("border-slate-900 text-center decoration-white")
+        .text("Review Score: ", reviewInfo.data.gamerscore);
 
-    if(reviewInfo.data.gamerscore === null || undefined) {
-    var gameReviewTitleEl = $('<p>').text('No review score was found.');
-    } else {
-      var gameReviewTitleEl = $('<p>').text('Review Score: ', reviewInfo.data.gamerscore);
- 
+      if (reviewInfo.data.gamerscore === null || undefined) {
+        var gameReviewTitleEl = $("<p>").text("No review score was found.");
+      } else {
+        var gameReviewTitleEl = $("<p>").text(
+          "Review Score: ",
+          reviewInfo.data.gamerscore
+        );
+      }
+      reviewContainerEl.append(gameReviewTitleEl);
+
+      //display individual reveiws with author, critic score, date pub, quote
+      var criticReviewContainer = $("<div>");
+      var criticReviewData = reviewInfo.data.game_critic_reviews;
+      criticReviewData.forEach(function (review) {
+        //author
+        var authorEl = $("<p>")
+          .addClass("border-slate-900 text-center decoration-white")
+          .text("Author: " + review.author);
+        criticReviewContainer.append(authorEl);
+        //critic score
+
+        var criticScoreEl = $("<p>")
+          .addClass("border-slate-900 text-center decoration-white")
+          .text("Score: score here");
+
+        var criticScoreEl = $("<p>")
+          .addClass("")
+          .text(
+            "Score: " + review.criticscore.score + "/" + review.criticscore.best
+          );
+
+        criticReviewContainer.append(criticScoreEl);
+        //date published
+        var publishedEl = $("<p>")
+          .addClass("border-slate-900 text-center decoration-white")
+          .text("Date Published: " + review.date_published);
+        criticReviewContainer.append(publishedEl);
+        //quote
+        var quoteEl = $("<p>")
+          .addClass("border-slate-900 text-center decoration-white")
+          .text("Quote: " + review.qoute);
+        criticReviewContainer.append(quoteEl);
+      });
+      reviewContainerEl.append(criticReviewContainer);
     }
-    reviewContainerEl.append(gameReviewTitleEl);
-
-    //display individual reveiws with author, critic score, date pub, quote
-    var criticReviewContainer = $('<div>');
-    var criticReviewData = reviewInfo.data.game_critic_reviews;
-    criticReviewData.forEach(function(review){
-      //author
-      console.log(review);
-      console.log(review.author);
-      var authorEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('Author: ' + review.author);
-      criticReviewContainer.append(authorEl);      
-      //critic score
-
-      var criticScoreEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('Score: score here');
-
-      var criticScoreEl = $('<p>').addClass('').text('Score: ' + review.criticscore.score + "/" + review.criticscore.best);
-
-      criticReviewContainer.append(criticScoreEl);  
-      //date published
-      var publishedEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('Date Published: ' + review.date_published);
-      criticReviewContainer.append(publishedEl);   
-      //quote
-      var quoteEl = $('<p>').addClass('border-slate-900 text-center decoration-white').text('Quote: ' + review.qoute);
-      criticReviewContainer.append(quoteEl); 
-    })
-    reviewContainerEl.append(criticReviewContainer);
   }
 }
-//fetches reveiw using game title as query (whattoplay API)
-//if response works, gameHandler
-//if 400, gameErrorHandler
-//if 200, gameConnectionErrorHandler
-
-//gameReviewHandler
-//save game to local storage (savedGamesObj)
-//display new saved game to the saved games buttons
-//if saved game already exists, dont create a new button
-//display game title
-//display game reveiw
